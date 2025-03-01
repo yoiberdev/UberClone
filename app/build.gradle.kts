a86ad413e6886344c80aac580945bbe71a43955b
Angierc25 <carhuas2005@gmail.com>
2025-02-28 21:38:27 -0500

    Agregando cambios adicionales en .idea y app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yoiberdev.uberclone"
-        minSdk = 35
+        minSdk = 21
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
